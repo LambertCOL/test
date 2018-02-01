a3876075d4d30ac0906fa2971a0b68d422b424e6
LambertCOL <1677183216@qq.com>
2018-02-01 22:13:29 +0800

    创建了方法func2_cb1

--- test/src/func2.java
+++ test/src/func2.java
@@ -5,4 +5,7 @@ public class func2 {
     public static void func2(){
         System.out.println("这是方法2");
     }
+    public static void func2_cb1(){
+        System.out.println("这是码农一更新的方法2");
+    }
 }
